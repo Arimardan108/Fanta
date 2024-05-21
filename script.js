51e26240603f4875dff38d7b4504d73563829719
Arimardan-mishra <101453479+Arimardan108@users.noreply.github.com>
2024-05-21 13:20:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ tl2.from(
   {
     rotate: "-90deg",
     top: "110%",
-    left: "0",
+    left: "100%",
   },
   "ca"
 );
@@ -95,7 +95,7 @@ tl2.from(
   {
     rotate: "90deg",
     top: "110%",
-    left: "0",
+    left: "100%",
   },
   "ca"
 );
